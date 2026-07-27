c22819a6e1548bbb059a350c05ee53d40dce89c2
andreimalvich <andreimalvich@gmail.com>
2026-07-27 19:57:53 +0300

    правка 1

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace BatchProcess3.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BatchProcess3.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
